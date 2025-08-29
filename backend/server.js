cf7c4d6c8c2b04c3281bdda45e0c1c08708cd1b9
Shailesh Kumar Yadav <yshailesh803@gmail.com>
2025-08-30 00:02:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use("/api/todos", todoRoutes)
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(Path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
